f312cbda5d8713953a1462dbd3594519c05b30b0
eunicebes <eunicebes@gmail.com>
2017-09-27 18:41:04 +0800

    Import user profile and posts into elasticsearch

--- import_data.py
+++ import_data.py
@@ -9,53 +9,45 @@ import os
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 folder = './data/'
 
-def read_post(posts):
-    post_arr = []
+def read_post(posts, user_id):
     for post in posts:
-        if 'comments' in post:
-            post_dic = {
-                'id': post['id'],
-                'story': post['story'],
-                'message': post['message'],
-                'created_time': post['created_time'],
-                'reactions': post['reactions']['data'],
-                'comments': post['comments']['data']
-            }
-            post_arr.append(post_dic)
-            break
-    print(post_arr)
+        post_dic = {
+            'user_id': user_id,
+            'id': post['id'],
+            'story': post['story'],
+            'message': post['message'],
+            'created_time': post['created_time'],
+            'reactions': post['reactions']['data']
+            #'comments': post['comments']['data'] if 'comments' in post else []
+        }
+        print(post_dic)
+        es.index(index="facebook", doc_type="post", id=post['id'], body=post_dic)
+
+
+gender_dict = {"男性":"male", "女性":"female", "male":"male", "female":"female"}
 
 #Retrieve data from file
 for file_name in os.listdir(folder):
     # print(file_name)
     with open(folder + file_name) as file:
-        for i, line in enumerate(file.readlines()):         
+        # print(file_name)
+        for i, line in enumerate(file.readlines()):
+            # print(i)
             try:
                 data = json.loads(line)
-                #print(data['id'])
-                post_dic =  read_post(data['posts']['data'])
-                break
-                # try:
-                #     document = {
-                #         'id': data['id'],
-                #         'name': data['name'],
-                #         'gender': data['gender']
-                #     }
-                #     print(document)
-                #     break
-                # except:
-                #     document = {
-                #         'id': data['id'],
-                #         'post':{
-
-                #         }
-                #     }
-                #     print(document)
-                #     break
-                # To remove the html tags
-                # data['content'] = ''.join(BeautifulSoup(data['content']).findAll(text=True))
+                # print(data['id'])
+                try:
+                    document = {
+                        'id': data['id'],
+                        'name': data['name'],
+                        'gender': gender_dict[data['gender']]
+                    }
+                    read_post(data['posts']['data'], data['id'])
+                    es.index(index="facebook", doc_type="user", id=data['id'], body=document)
+                except:
+                    read_post(data['posts']['data'], data['id'])
+                    continue
             except:
                 # print(line)
                 continue
-            # es.index(index="facebook", doc_type=file_name.split('.')[0], id=i+1, body=data)
-    break
+           
